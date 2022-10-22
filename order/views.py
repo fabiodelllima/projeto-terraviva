c6ea208b7cd39f1dbbd95fd8e0b26d04a3e0fe7b
fabiodelllima <f.f.fabiodelima@gmail.com>
2022-10-22 17:39:07 -0300

    refactor: code review

--- order/views.py
+++ order/views.py
@@ -13,6 +13,7 @@ from rest_framework.response import Response
 from .models import Order, OrderItem
 from .serializers import OrderSerializer, MyOrderSerializer
 
+
 @api_view(['POST'])
 @authentication_classes([authentication.TokenAuthentication])
 @permission_classes([permissions.IsAuthenticated])
@@ -21,8 +22,8 @@ def checkout(request):
 
     if serializer.is_valid():
         stripe.api_key = settings.STRIPE_SECRET_KEY
-        paid_amount = sum(item.get('quantity') * item.get('product').price for item in serializer.validated_data['items'])
-
+        paid_amount = sum(item.get(
+            'quantity') * item.get('product').price for item in serializer.validated_data['items'])
         try:
             charge = stripe.Charge.create(
                 amount=int(paid_amount * 100),
@@ -30,14 +31,12 @@ def checkout(request):
                 description='Charge from Djackets',
                 source=serializer.validated_data['stripe_token']
             )
-
             serializer.save(user=request.user, paid_amount=paid_amount)
-
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         except Exception:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class OrdersList(APIView):
     authentication_classes = [authentication.TokenAuthentication]
