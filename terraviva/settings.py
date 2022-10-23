b6fd46853eff9d2d2c738d0e7d1d8b438d9cbd66
fabiodelllima <f.f.fabiodelima@gmail.com>
2022-10-22 21:31:25 -0300

    add: settings for django-on-heroku

--- terraviva/settings.py
+++ terraviva/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ryrj&n6dw_p(!m&e(&cnyp0pn4tmx3#)^0bjl+yh$28o5h444h'
+# SECRET_KEY = 'django-insecure-ryrj&n6dw_p(!m&e(&cnyp0pn4tmx3#)^0bjl+yh$28o5h444h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
